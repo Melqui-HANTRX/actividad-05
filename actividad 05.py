55caa67d40d1f87735352d5a17ca3c69405a4c8d
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-22 09:49:37 -0600

    ciclo agregado

---
+++ actividad 05.py
@@ -0,0 +1,17 @@
+while True:
+ print("---MENÚ ANALISIS DE VENTAS---")
+
+ print("\n 1. Ingrese las ventas(solo valores enteros positivos)")
+ print("2. Ver todas las ventas ingresadas)")
+ print("3. Calcular la venta mas alta y la mas baja")
+ print("4. Calcular promedio de ventas")
+ print("5. Contar cuentos dias superaron los Q1000")
+ print("6. Clasificar cada venta:(>1000), media (500-1000), baja(<500)")
+ print("7. Salir")
+ menu = input("Ingrese la accion que desea realizar: ")
+ match menu:
+     case "1":
+      print("")
+
+
+
